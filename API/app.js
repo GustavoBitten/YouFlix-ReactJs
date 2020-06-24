d3e445a92a404f73fa6520e6b462dff9f41abd8b
GustavoBitten <gustavoduarte1999@hotmail.com>
2020-06-24 15:19:26 -0300

    improve api v1.1

--- API/app.js
+++ API/app.js
@@ -54,4 +54,78 @@ app.get('/main-videos', async (req,res)=>{
 
 
 
+async function autoScroll(page){
+    await page.evaluate(async () => {
+        await new Promise((resolve, reject) => {
+            var totalHeight = 0;
+            var distance = 10000;
+            var timer = setInterval(() => {
+                var scrollHeight = document.body.scrollHeight;
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+
+                if(totalHeight >= scrollHeight){
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 100);
+        });
+    });
+}
+
+
+const puppeteer = require('puppeteer');
+async function testArea() {
+
+    
+      const browser = await puppeteer.launch({
+          headless:false,
+          defaultViewport:null
+
+      });
+      const page = await browser.newPage();
+      await page.goto('https://www.youtube.com');
+      //await page.screenshot({path: 'hnw.png',fullPage: 'true'});
+      //await page.content;
+      await autoScroll(page)
+      await page.waitFor(1000)
+      await autoScroll(page)
+      await page.waitFor(1000)
+      await autoScroll(page)
+      await page.waitFor(1000)
+      await autoScroll(page)
+      await page.waitFor(1000)
+      await autoScroll(page)
+      await page.waitFor(1000)
+      await autoScroll(page)
+      await page.waitFor(1000)
+      await page.waitFor('ytd-rich-item-renderer')
+
+      const result = await page.$$eval('ytd-rich-item-renderer', box=>{
+          return box.map(row =>{
+              var content = {}
+              const title = row.querySelector('img')
+              try {
+                  content = title.getAttribute('src')
+                  return content
+                  
+              } catch (error) {
+                  content = ''
+                    return content
+              }
+          })
+      })
+      ;;
+      
+    //   const result3 = result.map(lem =>{
+    //       const tilte = lem.querySelector('.result-title')
+    //       const text = tilte.innerHTML
+    //       console.log(text)
+    //   })
+      //await browser.close();
+    await console.log(result);
+}
+
+testArea()
+
 app.listen(3333,()=>console.log('Api online'))
